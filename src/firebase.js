5e4b3492f33a0e28d27cbaa5becd4c87a83faa53
peter6426 <73055746+peter6426@users.noreply.github.com>
2021-05-27 02:17:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmd4zFOnrqTVa9PKZfqmylWCiwNOCjM9k",
-  authDomain: "whatsapp-mern-de006.firebaseapp.com",
-  projectId: "whatsapp-mern-de006",
-  storageBucket: "whatsapp-mern-de006.appspot.com",
-  messagingSenderId: "296971547507",
-  appId: "1:296971547507:web:02e7a2a0b9cea7d9e2cb06",
-  measurementId: "G-MTXJ94G4FX"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
